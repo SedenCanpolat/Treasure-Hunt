954d371f32509348cedbe289412e7660df21ad2d
NurCeylin <ceylincetin03@hotmail.com>
2024-03-14 14:19:22 +0300

    başlangıç haraket kodu

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb2D;
-    public float moveSpeed;
-    private float speedX, speedY;
+    public float speed = 10f;
+    Vector2 lastClickedPos;
+    bool moving;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
-        rb2D.velocity = new Vector2(speedX, speedY);
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+        if (moving && (Vector2)transform.position != lastClickedPos)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+        }
+        else
+        {
+            moving = false;
+        }
     }
 }
