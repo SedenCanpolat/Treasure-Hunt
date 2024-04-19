e66671eaf0ef433efbd7a1d0355292e6d814301f
Hir0zuki <s.iremozdemir@gmail.com>
2024-04-19 08:43:31 +0300

    efekt_ekleme1

---
+++ Assets/Scripts/vfx_script.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vfx_script : MonoBehaviour
+{
+    [SerializeField] GameObject Vfx;
+
+    Vector2 mouse;
+    void Start()
+    {
+        Vfx.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mouse();
+    }
+
+    private void Mouse()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            mouse=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vfx.SetActive(true);
+            Vfx.transform.position = new Vector3(mouse.x, mouse.y, 0f);
+        }
+        if(Input.GetMouseButtonUp(0)) {
+            Vfx.SetActive(false);
+        }
+    }
+}
