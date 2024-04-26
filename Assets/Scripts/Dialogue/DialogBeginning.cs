e92d8410404f8ae4443f050fbbd8a435af739ed8
NurCeylin <ceylincetin03@hotmail.com>
2024-04-26 14:01:06 +0300

    DialogBeginning

---
+++ Assets/Scripts/Dialogue/DialogBeginning.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogBeginning : MonoBehaviour
+{
+    public DialogTrigger trigger;
+    void Start()
+    {
+        trigger.StartDialogue();
+    }
+    void Update()
+    {
+
+    }
+
+}
+
