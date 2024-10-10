564273520c3ebb0b85391605d2463b64cb6bc424
NurCeylin <ceylincetin03@hotmail.com>
2024-10-10 18:16:29 +0300

    inventory alma eklendi
    inventory alma eklendi

---
+++ Assets/Scripts/InventorySystem/ItemMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMovement : MonoBehaviour
+{
+    public void OnMouseEnter()
+    {
+        //InventoryManager.instance.addItem(this.Image);
+        //Destroy(this);
+
+    }
+
+    public void OnBeginDrag()
+    {
+        Debug.Log("aaa");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
